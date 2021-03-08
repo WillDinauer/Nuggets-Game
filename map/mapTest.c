df731c1ac485b7dd9162e6832ec7076b19ee61e3
gusemmett <f003jdt@plank.thayer.dartmouth.edu>
2021-03-08 13:50:56 -0500

    Added movement testing

--- map/mapTest.c
+++ map/mapTest.c
@@ -6,6 +6,8 @@
 #include "hashtable.h"
 
 player_t *makePlayer();
+void randPos(position_t *pos);
+bool checkValidMove(map_t *map, player_t *p);
 
 int main(const int argc, const char *argv[])
 {
@@ -13,29 +15,70 @@ int main(const int argc, const char *argv[])
 	FILE *fp = fopen("../maps/main.txt", "r");
 
 	map_t *map = map_new(fp);
-
-	player_t *p = makePlayer();
-
 	printf("map width: %d, height: %d\n\n", map->width, map->height);
 
-	map_t *plyrMap = map_buildPlayerMap(map,p,NULL, NULL);
+
+	player_t *p = makePlayer();
+	position_t *pos = malloc(sizeof(position_t));
+	map_t *plyrMap;
+
+	for (int i = 0; i < 20; i++){
+		randPos(pos);
+		map_movePlayer(map, p, pos, hashtable_new(1));
+		plyrMap = map_buildPlayerMap(map,p,NULL, NULL);
+
+		if (checkValidMove(map,p)){
+			printf(" -- Valid Move\n");
+		} else {
+			printf(" -- Invalid Move\n");
+		}
+	}
+
 	printf("%s\n", plyrMap->mapStr);
-
-	printf("#######################################################\n");
-
+	
 }
 
 
 player_t *makePlayer()
 {
-
 	position_t *pos = malloc(sizeof(position_t));
 	pos->x = 6;
 	pos->y = 2;
 
 	player_t *p = malloc(sizeof(player_t));
-
 	p->pos = pos;
 
 	return p;
+}
+
+void randPos(position_t *pos)
+{
+
+	int mv = rand() % 3;
+	printf("Moving Player from: (%d,%d) to ", pos->x, pos->y);
+	// Vertical
+	if (mv == 0){
+		pos->y = rand() % 10;
+	} 
+	// Horizontal
+	else if (mv == 1){
+		pos->x = rand() % 10;
+	} 
+	// Diagonal
+	else{
+		pos->x = rand() % 10;
+		pos->y = rand() % 10;
+	}
+	printf("(%d,%d)", pos->x, pos->y);
+}
+
+
+bool checkValidMove(map_t *map, player_t *p)
+{
+
+	char c = map->mapStr[(p->pos->y * map->width) + (p->pos->x + 1)];
+	if (c != ' ' && c != '-' && c != '|' && c != '+'){
+		return true;
+	}
+	return false;
 }

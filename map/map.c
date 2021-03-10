6d005777bc5a878d9450c9aa8c49a43a2767d345
Jacob W. Dell <f003mjb@plank.thayer.dartmouth.edu>
2021-03-09 19:15:26 -0500

    map.c visibility changes (compiles; no bug fixes yet)

--- map/map.c
+++ map/map.c
@@ -9,14 +9,20 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 #include "map.h"
 #include "message.h"
 #include "hashtable.h"
 
 /**************** Private Functions ****************/
 map_t *map_copy(map_t *map);
-char *map_calculateVisibility(map_t *map, player_t *player, hashtable_t *goldData, hashtable_t *players);
+char *map_calculateVisibility(map_t *map, player_t *player, char *ogMapStr);
 bool canPlayerCanMoveTo(map_t *map, position_t *pos);
+void passageVis(char *currentVis, int i, map_t *map, char *mapstring, player_t *player);
+void wallChar(char *mapstring, char *currentVis, int index);
+bool isVisible(map_t *map, int i, player_t *player);
+bool checkObstruct(int i, map_t *map);
+
 /**************** Iterator Functions ****************/
 void addPlayerITR(void *arg, const char *key, void *item);
 void placeGold(void *arg, const char *key, void *item);
@@ -101,7 +107,9 @@ map_t *map_buildPlayerMap(map_t *map, player_t *player, hashtable_t *goldData, h
     }
     outMap->mapStr = map_buildOutput(outMap);
 
-	return outMap;
+    // figure out visibility and return the player's visibility mapstring
+	outMap->mapStr = map_calculateVisibility(outMap, player, map->mapStr);
+    return outMap;
 }
 
 void placeGold(void *arg, const char *key, void *item)
@@ -195,9 +203,229 @@ map_t *map_copy(map_t *map)
 
 
 /**************** map_calculateVisibility ****************/
-char *map_calculateVisibility(map_t *map, player_t *player, hashtable_t *goldData, hashtable_t *players)
+char *map_calculateVisibility(map_t *map, player_t *player, char *ogMapStr)
 {
-	return "Place Holder";
+    // get player position, map string, current
+    //  player visibility string
+	char *mapstring = map->mapStr;
+    int playerpos = map_calcPosition(map, player->pos);
+    char *currentVis = player->visibility;
+    
+    // check if this is a new player
+    //  and create visibility string if so
+    if (strcmp(currentVis, "") == 0) {
+        currentVis = map_buildOutput(map);
+        for (int i = strlen(mapstring); i >= 0; i--) {
+            if (mapstring[i] == '\n' || mapstring[i] == '\0') {
+                // keep important signpost characters
+                continue;
+            } else if (!isVisible(map, i, player)) {
+                // ditch everything else that's not visible
+                currentVis[i] = ' ';
+            }
+        }
+        return currentVis;
+    } else if (currentVis[playerpos] == '@') {
+        // position hasn't changed
+        // visibility stays the same
+        return currentVis;
+    }
+
+    // loop through previous visibility string
+    for (int i = strlen(currentVis); i >= 0; i--) {
+        if (currentVis[i] == '*' && !isVisible(map, i, player)) {
+            // reset out-of-sight gold visibility
+            currentVis[i] = '.';
+        } else if (isalpha(currentVis[i]) != 0 && !isVisible(map, i, player)) {
+            // reset out-of-sight player locations
+            if (ogMapStr[i] == '#') {
+                currentVis[i] = '#';
+            } else {
+                currentVis[i] = '.';
+            }
+        }
+    }
+
+    // loop through map to decide what
+    //  currentVis should contain
+    for (int i = strlen(mapstring); i >= 0; i--) {
+        if (mapstring[i] == '@') {
+            // make sure to include player position
+            currentVis[i] = '@';
+        } else if (mapstring[i] == '#') {
+            // figure out what's visible in the passage
+            passageVis(currentVis, i, map, mapstring, player);
+        } else if (mapstring[i] == currentVis[i]) {
+            // don't touch what's been seen
+            continue;
+        } else if (isVisible(map, i, player)) {
+            // add what's newly visible
+            currentVis[i] = mapstring[i];
+        } else {
+            // make sure rock is rock
+            //  and invisible is invisible
+            currentVis[i] = ' ';
+        }
+    }
+    return currentVis;
+}
+
+
+/**************** passageVis ****************/
+void passageVis(char *currentVis, int i, map_t *map, char *mapstring, player_t *player)
+{
+    if (isVisible(map, i, player)) {
+        currentVis[i] = '#';
+    } else {
+        // get position and then indices of the four
+        //  surrounding non-diagonal gridpoints
+        position_t *pos = map_intToPos(map, i);
+        int plusx, minusx, plusy, minusy;
+        pos->x += 1;
+        plusx = map_calcPosition(map, pos);
+        pos->x -= 2;
+        minusx = map_calcPosition(map, pos);
+        pos->y += 1;
+        plusy = map_calcPosition(map, pos);
+        pos->y -= 2;
+        minusy = map_calcPosition(map, pos);
+        
+        if (currentVis[plusx] == '@' ||
+            currentVis[minusx] == '@' ||
+            currentVis[plusy] == '@' ||
+            currentVis[minusy] == '@') {
+            // the player is next to this passage
+            //  gridpoint and should be able to see it
+            currentVis[i] = '#';
+            
+            // hand off to helper to figure out
+            //  whether to add wall characters
+            wallChar(mapstring, currentVis, plusx);
+            wallChar(mapstring, currentVis, minusx);
+            wallChar(mapstring, currentVis, plusy);
+            wallChar(mapstring, currentVis, minusy);
+        }
+    }
+}
+
+
+/**************** wallChar ****************/
+void wallChar(char *mapstring, char *currentVis, int index)
+{
+    // add in wall characters adjacent
+    //  to this passage gridpoint
+    switch (mapstring[index]) {
+        case '+' :
+            currentVis[index] = '+';
+            break;
+        case '-' :
+            currentVis[index] = '-';
+            break;
+        case '|' :
+            currentVis[index] = '|';
+            break;
+    }
+}
+
+
+/**************** isVisible ****************/
+bool isVisible(map_t *map, int i, player_t *player)
+{
+    bool vis = true;
+
+    // get position of the point in question
+    position_t *gridpoint = map_intToPos(map, i);
+    
+    // establish slope m and constant b according to
+    //  the standard linear equation y = mx + b
+    //  from the player's position and that of the
+    //  gridpoint in question
+    int rise, run;
+    float slope, constant;
+    rise = player->pos->y - gridpoint->y;
+    run = player->pos->x - gridpoint->x;
+    slope = rise / run;
+    constant = player->pos->y - (player->pos->x*slope);
+    
+    // loop through map to look for obstructions
+    for (int j = strlen(map->mapStr); i >= 0; i--) {
+        position_t *thispos = map_intToPos(map, j);
+        // check whether this point is between
+        //  the point of interest and the player's position
+        if (!(((thispos->x <= player->pos->x &&
+                thispos->x >= gridpoint->x) ||
+               (thispos->x >= player->pos->x &&
+                thispos->x <= gridpoint->x)) &&
+              ((thispos->y <= player->pos->y &&
+                thispos->y >= gridpoint->y) ||
+               (thispos->y >= player->pos->y &&
+                thispos->y <= gridpoint->y)))) {
+            // not in the intervening rows/columns
+            //  so not a concern for obstruction
+            continue;
+
+        } else if ((thispos->x*slope) + constant == (float)thispos->y) {
+            // point directly on line; check if it obstructs
+            if (checkObstruct(j, map)) {
+                vis = false;
+            }
+        } else {
+            // prepare quantities to compare to thispos->y
+            float xslopeConst, plusxslopeConst, minusxslopeConst;
+            xslopeConst = (thispos->x*slope) + constant;
+            plusxslopeConst = ((thispos->x + 1)*slope) + constant;
+            minusxslopeConst = ((thispos->x - 1)*slope) + constant;
+
+            // various ways in which the line might pass between
+            //  obstructing gridpoints; check obstruction accordingly
+            if (xslopeConst <= (float)thispos->y &&
+                plusxslopeConst >= (float)thispos->y) {
+                thispos->x += 1;
+                if (checkObstruct(j, map) &&
+                    checkObstruct(map_calcPosition(map, thispos), map)) {
+                    vis = false;
+                }
+            } else if (xslopeConst >= (float)thispos->y &&
+                       plusxslopeConst <= (float)thispos->y) {
+                thispos->x += 1;
+                if (checkObstruct(j, map) &&
+                    checkObstruct(map_calcPosition(map, thispos), map)) {
+                    vis = false;
+                }
+            } else if (xslopeConst <= (float)thispos->y &&
+                       minusxslopeConst >= (float)thispos->y) {
+                thispos->x -= 1;
+                if (checkObstruct(j, map) &&
+                    checkObstruct(map_calcPosition(map, thispos), map)) {
+                    vis = false;
+                }
+            } else if (xslopeConst >= (float)thispos->y &&
+                       minusxslopeConst <= (float)thispos->y) {
+                thispos->x -= 1;
+                if (checkObstruct(j, map) &&
+                    checkObstruct(map_calcPosition(map, thispos), map)) {
+                    vis = false;
+                }
+            }
+        }
+    }
+    return vis;
+}
+
+
+/**************** checkObstruct ****************/
+bool checkObstruct(int i, map_t *map)
+{
+    bool obstruct = false;
+    switch (map->mapStr[i]) {
+        case '+' :
+        case '-' :
+        case '|' :
+        case '#' :
+        case ' ' :
+            obstruct = true;
+    }
+    return obstruct;
 }
 
 
